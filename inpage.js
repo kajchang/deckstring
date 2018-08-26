fce45c333a90d065500224a8af7e75a2cee095f5
Kai Chang <kaijchang@gmail.com>
2018-08-26 14:29:41 -0700

    basic web demo

---
+++ inpage.js
@@ -0,0 +1,114 @@
+$("#cardsearch").on("input", function() {
+    $("#results").empty();
+    if ($(this).val() !== "") {
+        deckstring.cards.forEach(card => {
+            if (card["name"].toLowerCase().includes($(this).val().toLowerCase())) {
+                $(`<tr name="` + card["id"] + `">
+                    <td><img class="mr-2" src="images/` + card["aspect"][0] + `.png" width="20" height="20"/><a href="#">` + card["name"] + `<img src="images/` + card["id"] + `.png"/></a></td>
+                    <td>` + card["cost"] + `</td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-light" onclick="addCard($(this).parent().parent().attr('name'));">
+                            <span class="fa fa-plus" aria-hidden="true"></span>
+                        </button>
+                    </td>
+                   </tr>`).hide().appendTo($("#results")).show("normal");
+            }
+        });
+    }
+});
+
+function addCard(card_id) {
+    const cardCount = countCards();
+
+    if (cardCount < 30) {
+        for (let card of deckstring.cards) {
+            if (card["id"] == card_id) {
+                const aspects = deckAspects();
+                if (aspects.length < 2 | aspects.includes(card["aspect"][0])) {
+                    var found = false;
+    
+                    for (let tr of $("#deck > tr")) {
+                        if ($(tr).attr("name") == card_id) {
+                            if ($(tr).find("input").val() <= 2) {
+                                $(tr).find("input").val(parseInt($(tr).find("input").val()) + 1);
+                            }
+                            found = true;
+                        }
+                    }
+        
+                    if (!found) {
+                        $(`<tr name="` + card["id"] + `">
+                            <td><img class="mr-2" src="images/` + card["aspect"][0] + `.png" width="20" height="20"><a href="#">` + card["name"] + `<img src="images/` + card["id"] + `.png"></a></td>
+                            <td>` + card["cost"] + `</td>
+                            <td><input type="number" min="0" max="3" placeholder="#" step="1" value="1"/></td>
+                            <td>
+                                <button type="button" class="btn btn-sm btn-light" onclick="$(this).parent().parent().remove();calculateDeckstring();">
+                                    <span class="fa fa-minus" aria-hidden="true"></span>
+                                </button>
+                            </td>
+                           </tr>`).hide().appendTo($("#deck")).show("normal");
+        
+                        $("#deck > tr > td > input").change(function() {
+                            if ($(this).val() == 0) {
+                                $(this).parent().parent().remove();
+                            }
+                            calculateDeckstring();
+                        });
+                    }
+                    
+                    calculateDeckstring();
+
+                    break;
+
+                } else {
+                    // non-obtrusive alert in future
+                    alert("Only two aspects per deck");
+                }
+            }
+        }
+    } else {
+        // non-obtrusive alert in future
+        alert("Only 30 cards per deck");
+    }
+}
+
+function countCards() {
+    var cards = 0;
+
+    for (let tr of $("#deck > tr")) {
+        cards += parseInt($(tr).find("input").val());
+    }
+
+    return cards;
+}
+
+function deckAspects() {
+    const aspects = [];
+
+    for (let tr of $("#deck > tr")) {
+        var aspect = /[ACDNT]/.exec($(tr).find("img").attr("src"))[0];
+        if (!aspects.includes(aspect)) {
+            aspects.push(aspect);
+        }
+    }
+
+    return aspects;
+}
+
+function calculateDeckstring() {
+    $("#deckstring").val(deckstring.encode_deck($("#deck > tr").toArray().map(tr => [parseInt($(tr).attr("name")), parseInt($(tr).find("input").val())]), deckAspects().map(aspect => 'ACDNT'.indexOf(aspect))));
+}
+
+$("#deckstring").on("input", function () {
+    $("#deck").empty();
+
+    const deck = deckstring.decode_deck($(this).val());
+
+    deck[2].forEach(card => {
+        for (let x=0; x < card[1]; x++) {
+            addCard(card[0]);
+        }
+    });
+});
+
+calculateDeckstring();
